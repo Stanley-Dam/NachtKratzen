554cf9753f1c8d607aab9795e6da871233c26711
kevin <kevinb2002@live.nl>
2020-05-11 18:41:59 +0200

    Added working 24-hour clock

---
+++ Assets/DayNightCycle/Scripts/TimeClock.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeClock : MonoBehaviour
+{
+    [SerializeField]
+    private float clockHour, clockMin, clockSec;
+
+    [SerializeField]
+    TimeScript timeScript;
+
+    [SerializeField]
+    Text rawClockTXT, digitalClockTXT;
+
+    private void Update()
+    {
+        clockHour = Mathf.Floor(timeScript.GetTime() / 3600);
+        clockMin = Mathf.Floor(timeScript.GetTime() / 60 - clockHour * 60);
+        clockSec = Mathf.Floor(timeScript.GetTime() - clockMin * 60 - clockHour * 3600);
+
+        rawClockTXT.text = "Raw time: " + Mathf.Floor(timeScript.GetTime());
+        digitalClockTXT.text = "Digital Time: " + clockHour + ":" + clockMin + ":" + clockSec;
+    }
+}
--- Assets/DayNightCycle/Scripts/TimeSunScript.cs
+++ Assets/DayNightCycle/Scripts/TimeSunScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SunScript : MonoBehaviour
+public class TimeSunScript : MonoBehaviour
 {
     [SerializeField]
     TimeScript timeScript;
