d5630c81093c221958153e4c204a3875229c5233
Stanley-Dam <42673142+Stanley-Dam@users.noreply.github.com>
2020-05-11 11:30:44 +0200

    Added local player move event
    Other components will now be able to gather movement information via a localPlayerMove event!

---
+++ Assets/Entities/LocalBodyObjects.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalBodyObjects : MonoBehaviour {
+
+    //Just holds player body information
+    public Transform head;
+    public Transform cameraHolder;
+
+}
