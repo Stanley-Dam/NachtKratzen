7d22ac11ff5983ea16eca1bbeba6d5697a7b715b
kevin <kevinb2002@live.nl>
2020-05-13 16:06:43 +0200

    Fixed bug with nachtkrab not having a line renderer

---
+++ Assets/Entities/096behaviour/IsRenderingScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsRenderingScript : MonoBehaviour
+{
+    private GameObject test;
+
+    private void OnBecameVisible()
+    {
+        print("I am visible");
+    }
+
+    private void OnBecameInvisible()
+    {
+        print("I am invisible");
+    }
+}
