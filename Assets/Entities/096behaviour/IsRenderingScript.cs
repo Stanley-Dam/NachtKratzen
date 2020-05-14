29256fa8eafe2730a857632c86ff2a274fcab47b
Stanley-Dam <42673142+Stanley-Dam@users.noreply.github.com>
2020-05-14 12:12:01 +0200

    Fixed seeker error

---
+++ Assets/Entities/096behaviour/IsRenderingScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsRenderingScript : MonoBehaviour
+{
+    public delegate void IsVisibleEvent(bool isNachtKrabVisible);
+    public static event IsVisibleEvent isVisibleEvent;
+
+    private bool isVisible;
+
+    private void OnBecameVisible()
+    {
+        isVisible = true;
+        if(isVisibleEvent != null)
+            isVisibleEvent(isVisible);
+    }
+
+    private void OnBecameInvisible()
+    {
+        isVisible = false;
+        if(isVisibleEvent != null)
+            isVisibleEvent(isVisible);
+    }
+}
--- Assets/Networking/objects/Seeker.cs
+++ Assets/Networking/objects/Seeker.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class Seeker : Player {
 
     private void Awake() {
-        RaycastPickup.foundPlayerEvent += playerGameObject => this.networkManager.KillPlayer(playerGameObject.GetComponent<Hider>().ClientId);
+        RaycastPickup.foundPlayerEvent += KillPlayer;
+    }
+
+    private void OnDisable() {
+        RaycastPickup.foundPlayerEvent -= KillPlayer;
+    }
+
+    private void KillPlayer(GameObject playerGameObject) {
+        if(playerGameObject.GetComponent<Hider>())
+            this.networkManager.KillPlayer(playerGameObject.GetComponent<Hider>().ClientId);
     }
 
 }
